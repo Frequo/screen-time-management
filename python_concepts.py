d80f3beea38f30a207bf2b28e4bef48c73000769
Frequo <Miazhang205@gmail.com>
2026-02-05 18:21:29 -0800

    Create python_concepts.py

---
+++ python_concepts.py
@@ -0,0 +1,118 @@
+myvar = 12
+print(5//2)
+
+myfloat = 12.5
+
+mybool = True
+
+mystring1 = "hello"
+
+mystring2 = "hello"
+
+
+print(myvar)
+
+## Conditionals
+x=7
+if 5 < x or myvar ==12:
+    print (":p")
+# and: both sides MUST be True
+# or: either can be True
+# xor: only one is allowed to be true 
+
+#comparison operators: < > == <= >=
+# not 
+
+print(( 4 < 5 ))
+print(not( 4 < 5 ))
+
+# Arithmetric Operators: + - * ** / // %
+print (( 30//27 ) * 10)
+print(5%2 == 1)
+
+print("🥶🫥")
+
+isEven = (x is 2 == 0)
+#isEven = (1 ==0)
+#is Even = false
+print (isEven) # True? False?
+
+myVar = 10
+
+# Data Structures
+#         0,1,2,3,4,5,6,7,8,9
+myList = [1,2,3,4,5,6,7,8,9,10]
+
+
+print(myList[3])
+
+
+# Loops
+while x < 10:
+    print("hi")
+    x = x + 1
+    
+for i in range(10):
+    print("ey")
+# for
+
+# int, float, bool, string, char,...
+myvar = 12
+
+print(5//2)
+
+myfloat = 12.5
+
+mybool = True
+
+mystring1 = "hello"
+
+mystring2 = "hello"
+
+
+print(myvar)
+
+## Conditionals
+x=7
+if 5 < x or myvar ==12:
+    print (":p")
+# and: both sides MUST be True
+# or: either can be True
+# xor: only one is allowed to be true 
+
+#comparison operators: < > == <= >=
+# not 
+
+print(( 4 < 5 ))
+print(not( 4 < 5 ))
+
+# Arithmetric Operators: + - * ** / // %
+print (( 30//27 ) * 10)
+print(5%2 == 1)
+
+print("🥶🫥")
+
+isEven = (x is 2 == 0)
+#isEven = (1 ==0)
+#is Even = false
+print (isEven) # True? False?
+
+myVar = 10
+
+# Data Structures
+#         0,1,2,3,4,5,6,7,8,9
+myList = [1,2,3,4,5,6,7,8,9,10]
+
+
+print(myList[3])
+
+
+# Loops
+while x < 10:
+    print("hi")
+    x = x + 1
+    
+for i in range(10):
+    print("ey")
+# for
+
